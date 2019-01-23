5b7e88df46b2c7d792344c78d50963333e408c15
LeoniePeters <leoniepeters@gmail.com>
2019-01-23 17:07:44 +0100

    finish project

--- src/components/DogsListContainer.js
+++ src/components/DogsListContainer.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import * as request from 'superagent'
 import DogsList from './DogsList'
 
+
 export default class DogsListContainer extends Component {
     state = { dogBreeds: null }
 
